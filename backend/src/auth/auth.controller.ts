1e46b81c5bcd2c8a341edf615f351e904d711611
guy <guynkama@gmail.com>
2023-05-18 07:15:58 +0100

    add auth controller

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    // Validate user credentials
+    const user = await this.authService.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException('email ou mot de passe incorrect');
+    }
+
+    // Generate and return the access token
+    const accessToken = await this.authService.signIn(user);
+    return accessToken;
+  }
+}
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './JwtStrategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService]
+  providers: [AuthService],
+  controllers: [AuthController]
 })
 export class AuthModule {}
